4d7ed23871b268727c9e4b5ff0f536b9d041f81b
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-12-30 20:01:02 -0700

    test: Add image test for LandingPage

--- src/pages/LandingPage/__tests__/LandingPage.tests.tsx
+++ src/pages/LandingPage/__tests__/LandingPage.tests.tsx
@@ -3,21 +3,28 @@ import { render, screen } from '@testing-library/react';
 import LandingPage from '../LandingPage';
 import { copyContent } from 'shared';
 
-const { heading, subHeading } = copyContent.landingPage;
+const { heading, subHeading, imageAlt } = copyContent.landingPage;
 
 describe('LandingPage tests', () => {
-  it('Should have a heading', () => {
+  beforeEach(() => {
     render(<LandingPage />);
+  });
+
+  it('Should have a heading', () => {
     const headingElement = screen.getByText(heading);
 
     expect(headingElement).toBeInTheDocument();
   });
 
   it('Should have a sub-heading', () => {
-    render(<LandingPage />);
-
     const subHeadingElement = screen.getByText(subHeading);
 
     expect(subHeadingElement).toBeInTheDocument();
+  });
+
+  it('Should have an image', () => {
+    const imageElement = screen.getByAltText(imageAlt);
+
+    expect(imageElement).toBeInTheDocument();
   });
 });
