95924bc74e6c3b579ffa43660d0ab7caf86f2743
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-12-30 20:43:11 -0700

    chore: Fix index files; add comments for context

--- src/shared/data/index.ts
+++ src/shared/data/index.ts
@@ -1,3 +1,8 @@
 import copyContent from './copyContent';
 
-export default { copyContent };
+// This is our standard pattern, but should only be used with
+// multiple "pages"; Remove the eslint-disable line once another
+// export is added to this file
+
+/* eslint-disable-next-line import/prefer-default-export */
+export { copyContent };
