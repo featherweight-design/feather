27dba225e355377c4b00d409f86cafc8b97a78ac
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-12-30 19:55:01 -0700

    style: Add initial layout styles to LandingPage

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,13 +1,18 @@
 import { FC } from 'react';
 
 import { copyContent } from 'shared';
+import './LandingPage.scss';
 
 const { heading, subHeading } = copyContent.landingPage;
 
 const LandingPage: FC = () => (
   <div className="app-initials-landing-page">
-    <h1>{heading}</h1>
-    <p>{subHeading}</p>
+    <h1 className="app-initials-landing-page__heading">{heading}</h1>
+    <p className="app-initials-landing-page__sub-heading">{subHeading}</p>
+    <img
+      className="app-initials-landing-page__image"
+      src={`${process.env.PUBLIC_URL}/assets/logo512.png`}
+    />
   </div>
 );
 
