2c7c2951606a74a111cf82f8e82fdff065942725
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-12-30 20:14:44 -0700

    chore: Add airbnb to eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,20 +18,29 @@ module.exports = {
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'airbnb', // Enables airbnb eslint rules (https://www.npmjs.com/package/eslint-config-airbnb)
   ],
   plugins: ['react-hooks'],
   rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // Place to specify ESLint rules. Can be used to overwrite rules
+    // specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     'no-use-before-define': 'off', // Turned off due to conflict with the below rule, which should be used with TS
     '@typescript-eslint/no-use-before-define': ['error', { variables: false }],
     'react-hooks/rules-of-hooks': 'error',
     'react/jsx-uses-react': 'off', // No longer needed as of React v17
     'react/react-in-jsx-scope': 'off', // No longer needed as of React v17
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ], // Allows JSX to be used in .ts/.tsx files
   },
   settings: {
     react: {
       version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+    'import/resolver': {
+      typescript: {}, // This loads <rootdir>/tsconfig.json to eslint
     },
   },
   overrides: [
