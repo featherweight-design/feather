3e2c6b0e5ceaa2a77a4fb58a8535a47b39a7ce2f
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-01-03 10:08:20 -0700

    chore: Update note in data/index.ts

--- src/shared/data/index.ts
+++ src/shared/data/index.ts
@@ -1,7 +1,7 @@
 import copyContent from './copyContent';
 
 // This is our standard pattern, but should only be used with
-// multiple "pages"; Remove the eslint-disable line once another
+// multiple data objects; Remove the eslint-disable line once another
 // export is added to this file
 
 /* eslint-disable-next-line import/prefer-default-export */
